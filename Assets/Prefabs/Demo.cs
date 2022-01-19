b70e0faccfdf24059f4e74b2a2506ed077e66dc2
FilipeCapInt <filipe@capitol-interactive.com>
2022-01-19 14:34:25 -0500

    Tablet Mode up

---
+++ Assets/Prefabs/Demo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AstroForge))]
+public class Demo : Editor
+{
+    public Texture tex;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        
+        GUILayout.BeginHorizontal();
+
+        
+
+        if (GUILayout.Button("Cycle"))
+        {
+            //maxHealth = EditorGUILayout.IntField("Health", maxHealth);
+        }
+
+        if (GUILayout.Button(tex))
+        {
+
+        }
+
+        if (GUILayout.Button(tex))
+        {
+
+        }
+
+        GUILayout.EndHorizontal();
+    }
+}
---
+++ Assets/Prefabs/Traits.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traits : MonoBehaviour
+{
+    [SerializeField]
+    private int maxHealth = 100;
+
+    [SerializeField]
+    [Range(1, 10)]
+    private int damagePerSecond = 3;
+
+    [SerializeField]
+    private string planetName;
+
+    [SerializeField]
+    [TextArea]
+    //[Multiline]
+    private string planetDescription;
+}
